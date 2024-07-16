6a03f7517f21a2af5e51bc217366f2869510af16
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2024-07-15 22:22:38 -0300

    fix: second chance position

--- src/table.c
+++ src/table.c
@@ -132,13 +132,7 @@ Frame *get_free_frame(Frame **frames, Table *table)
         {
             oldest_allocated = frame;
         }
-        if(SECOND_CHANCE){
-        if(frame->reference == 0){
-            return frame;
-        }else{
-            frame->reference = 0;
-        }
-        }
+      
     }
     switch (table->policy){
     case LRU:
@@ -146,7 +140,13 @@ Frame *get_free_frame(Frame **frames, Table *table)
     case FIFO:
         return oldest_allocated;
     case SECOND_CHANCE:
-
+        for(int i = 0; i < num_frames; i++){
+            Frame *frame = frames[i];
+            if(frame->reference == 0){
+                return frame;
+            }
+            frame->reference = 0;
+        }
     case RANDOM:
         return frames[rand() % num_frames];
     default:
